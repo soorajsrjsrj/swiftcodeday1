f1041cfe50204a43459a152e17afdaa2c6d498e1
MacStudent <macstudent@126-imac29.local>
2019-02-07 19:34:01 -0500

    Initial Commit

---
+++ NewFile/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NewFile
+//
+//  Created by MacStudent on 2019-02-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
