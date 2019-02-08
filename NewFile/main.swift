66e0a6e36a83e2ffed7ee670a0bc25676cbb2507
MacStudent <macstudent@126-imac29.local>
2019-02-07 21:05:58 -0500

    Da

--- NewFile/main.swift
+++ NewFile/main.swift
@@ -7,6 +7,41 @@
 //
 
 import Foundation
+var x:Int=5
 
-print("Hello, World!")
+
+for i in 1..<x{
+    print(i)
+}
+
+print("**************************************")
+for i in stride(from: x, to: 2, by: -1){
+    print(i)
+}
+
+print("**************************************")
+for i in stride(from: 1, to: 11, by: 1){
+    print("\(i) * 5 =",i*x)
+    
+}
+print("**************************************")
+for i in 1...10{
+    print("\(i) * 5 =",i*x)
+    
+}
+var _ = 100
+var p=(a:100,(10,29),b:(10,20))
+print(p.0)
+print(p.1.1)
+var t=(1,2)
+var(_,s)=t
+print(s)
+
+ var 😀="smile"
+print(😀)
+
+
+
+
+
 
